e4dfc2a9d062cf573dbf3b0b9c4417e3b10f3daa
Марк Ротач <rotachmark2001@mail.ru>
2025-03-02 16:51:00 +0500

    Удален лишний ViewModel

--- WeatherArchive/Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace WeatherArchive.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
