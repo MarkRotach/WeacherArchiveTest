b36285b7228e976a7f06b046cad6d8b4cdb522fc
Марк Ротач <rotachmark2001@mail.ru>
2025-03-02 16:53:27 +0500

    Обновлен HomeController

--- WeatherArchive/Controllers/HomeController.cs
+++ WeatherArchive/Controllers/HomeController.cs
@@ -1,25 +1,32 @@
-using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
-using WeatherArchive.Models;
 using WeatherArchive.Services;
 
 namespace WeatherArchive.Controllers;
 
-public class HomeController : Controller
+public class HomeController(
+    ILogger<HomeController> logger,
+    ArchiveUploader archiveUploader,
+    ArchiveService archiveService)
+    : Controller
 {
-    private readonly ILogger<HomeController> _logger;
-    private readonly ArchiveUploader _archiveUploader;
-
-    public HomeController(ILogger<HomeController> logger,
-        ArchiveUploader archiveUploader)
-    {
-        _logger = logger;
-        _archiveUploader = archiveUploader;
-    }
+    private readonly ILogger<HomeController> _logger = logger;
+    private const int PageSize = 20;
 
     public IActionResult Index()
     {
         return View();
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> ArchiveAsync(int page = 1, string month = "", int? year = null,
+        CancellationToken cancellationToken = default)
+    {
+        var archive = await archiveService.GetWeatherArchiveAsync
+        (
+            page, PageSize, month, year, cancellationToken
+        );
+        
+        return View(archive);
     }
 
     [HttpGet]
@@ -36,16 +43,23 @@ public class HomeController : Controller
             var extension = Path.GetExtension(file.FileName);
         
             if (extension is not (".xlsx" or ".xls"))
-                ModelState.AddModelError("", $"Недопустимый формат файла \"{file.FileName}\"!");
+                ModelState.AddModelError("error", $"Недопустимый формат файла \"{file.FileName}\"!");
         }
         
         if (!ModelState.IsValid)
             return View();
 
-        foreach (var file in files)
+        try
         {
-            var fileStream = file.OpenReadStream();
-            await _archiveUploader.UploadAsync(fileStream);
+            foreach (var file in files)
+            {
+                var fileStream = file.OpenReadStream();
+                await archiveUploader.UploadAsync(fileStream);
+            }
+        }
+        catch (Exception e)
+        {
+            ModelState.AddModelError("error", "Произошла ошибка обработки файлов!");
         }
         
         return View();
